33c818c8a7f61772d5cf56a43b4b282824417b42
VElysianP <byaoyi@seas.upenn.edu>
2017-09-01 12:05:39 -0400

    Sep1St

--- HoudiniWork1/FastPoissonDisk.cpp
+++ HoudiniWork1/FastPoissonDisk.cpp
@@ -1,7 +1,9 @@
 
 #include "FastPoissonDisk.h"
 
-FastPoissonDisk::FastPoissonDisk()
+//initialization, makes all of the data -1
+FastPoissonDisk::FastPoissonDisk() : 
+	radius(sqrt(DIMENSIONS_N) / MAX_SAMPLES), cellWidth(1 / MAX_SAMPLES), sampleAmount(0)
 {
 	for (int i = 0;i < MAX_SAMPLES;i++)
 	{
@@ -10,17 +12,18 @@ FastPoissonDisk::FastPoissonDisk()
 			for (int k = 0;k < MAX_SAMPLES;k++)
 			{
 				grid[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k] = -1;
-				samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].x = -1;
-				samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].y = -1;
-				samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].z = -1;
+				activeList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k] = -1;
+				samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].x = -1.0f;
+				samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].y = -1.0f;
+				samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].z = -1.0f;
 			}
 		}
 	}
 }
 
-//If the cell is empty, return true. If it is not empty, it should return the index of the cell
+//If the cell is empty, return -1. If it is not empty, it should return the index of the cell
 //Also the index of the grid should be (k,j,i)
-int FastPoissonDisk::SearchActiveList(int i, int j, int k)
+int FastPoissonDisk::SearchGridList(int i, int j, int k)
 {
 	//if the grid is empty then the number inside should be -1
 	int gridIndex = i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k;
@@ -48,9 +51,70 @@ Eigen::Vector3f FastPoissonDisk::GenerateOriginalSamplePoints()
 //radius between r and 2r should test the grid between (i-2,j-2,k-2) to (i+2,j+2,k+2)
 Eigen::Vector3f FastPoissonDisk::GenerateOtherSamples(Eigen::Vector3f testPoint)
 {
-	Eigen::Vector3i gridLocation = GridLocation(testPoint);
+	Eigen::Vector3i gridLocationTestPoint = GridLocation(testPoint);
+
+	int sampleTimeCount = 0;
+	while (sampleTimeCount < MAX_TEST_K)
+	{
+		sampleTimeCount++;
+
+		//the coordinates of the samplePoint is between 0.0 and 5*cellWidth
+		Eigen::Vector3f samplePoint;
+		samplePoint.x = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))*5.0f*cellWidth;
+		samplePoint.y = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))*5.0f*cellWidth;
+		samplePoint.z = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))*5.0f*cellWidth;
+
+		//start sample from (i-2,j-2,k-2)
+
+		Eigen::Vector3i samplePointGridLocation = GridLocation(samplePoint);
+
+		samplePointGridLocation.x = samplePointGridLocation.x + gridLocationTestPoint.x - 2;
+		samplePointGridLocation.y = samplePointGridLocation.y + gridLocationTestPoint.y - 2;
+		samplePointGridLocation.z = samplePointGridLocation.z + gridLocationTestPoint.z - 2;
+
+		//we do not have to check for the testPoint cell
+		if (samplePointGridLocation==gridLocationTestPoint)
+		{
+			continue;
+		}
+
+		//if the grid here is empty, which means the number inside the grid is -1
+		if (SearchGridList(samplePointGridLocation.z, samplePointGridLocation.y, samplePointGridLocation.x) == -1)
+		{
+
+		}
+		//which means the cell is occupied
+		else
+		{
+			continue;
+		}
+
+	}
+	
+	
 
 }
+
+
+//false means that the distance between the testPoint and the sample inside cell gridCoord is larger than radius
+//true means that the distance between the testPoint and the sample inside cell gridCoor is smaller than radius
+//which means that we should not add this point into sample list 
+bool FastPoissonDisk::CheckWitinRadius(Eigen::Vector3f testPoint, Eigen::Vector3i gridCoord)
+{
+	int sampleIndex = SearchGridList(gridCoord.z, gridCoord.y, gridCoord.x);
+
+	if (sampleIndex == -1)
+	{
+		return false;
+	}
+	else
+	{
+		Eigen::Vector3f thisSample = samplePointsList[sampleIndex];
+		if((testPoint-thisSample))
+	}
+}
+
+
 
 //sampleCount here starts at 0
 void FastPoissonDisk::InsertIntoGridAndSampleList(Eigen::Vector3f newSamplePoint,int sampleCount)
@@ -66,11 +130,38 @@ void FastPoissonDisk::InsertIntoGridAndSampleList(Eigen::Vector3f newSamplePoint
 	Eigen::Vector3i gridLocation = GridLocation(newSamplePoint);
 	
 	grid[GridArrayIndex(gridLocation)] = sampleCount;
+	sampleAmount++;
 }
 
+//sampleCount is the index of the sample that will be inserted inside the activeList
+void FastPoissonDisk::InsertIntoActiveList(int sampleCount)
+{
+	if (sampleCount > (MAX_SAMPLES*MAX_SAMPLES*MAX_SAMPLES))
+	{
+		return;
+	}
+	int insertIndex = CheckActiveListSize();
+	activeList[insertIndex] = sampleCount;
+}
+
+//this function will check how many samples indicies are there inside this active list
+//and it will return the size of the active list
+//which means the next index we can insert would be i
+//if active list has nothing, it will return 0
+int FastPoissonDisk::CheckActiveListSize()
+{
+	int i = 0;
+	while (activeList[i] > -1)
+	{
+		i++;
+	}
+	return i;
+}
+
+//the grid location start from (0,0,0)
 Eigen::Vector3i FastPoissonDisk::GridLocation(Eigen::Vector3f newSamplePoint)
 {
-	Eigen::Vector3i gridLocation = Eigen::Vector3i((int)(newSamplePoint.x / cellWidth), (int)(newSamplePoint.y / cellWidth), (int)(newSamplePoint.x / cellWidth));
+	Eigen::Vector3i gridLocation = Eigen::Vector3i(floor(newSamplePoint.x / cellWidth), floor(newSamplePoint.y / cellWidth), floor(newSamplePoint.z / cellWidth));
 	return gridLocation;
 }
 
@@ -80,32 +171,52 @@ int FastPoissonDisk::GridArrayIndex(Eigen::Vector3i gridLocation)
 	return girdArrayIndex;
 }
 
+//the remove Index start from 0, it should be the index in the active list
+void FastPoissonDisk::removeFromActiveList(int removeIndex)
+{
+	if (removeIndex > (MAX_SAMPLES*MAX_SAMPLES*MAX_SAMPLES))
+	{
+		return;
+	}
+
+	int i = removeIndex;
+	int activeListSize = CheckActiveListSize();
+	for (int i = removeIndex;i < activeListSize;i++)
+	{
+		activeList[i] = activeList[i + 1];
+	}
+}
+
 void FastPoissonDisk::GenerationProcess()
 {
-	int sampleCount = -1;
 	Eigen::Vector3f firstSample = GenerateOriginalSamplePoints();
-	sampleCount++;
+	sampleAmount++;
+	int sampleCount = sampleAmount - 1;
 
+	//insert the first sample into three lists
 	InsertIntoGridAndSampleList(firstSample, sampleCount);
+	InsertIntoActiveList(sampleCount);
 
+	int activeListSize = CheckActiveListSize();
+
+	while (activeListSize !=0)
+	{
+		//randomIndex start from 0
+		int randomIndex = (int)((static_cast <float> (rand()) / static_cast <float> (RAND_MAX))*(activeListSize-1));
+
+	}
 
 
 }
 
 void FastPoissonDisk::PrintSampleLocations()
 {
-	for (int i = 0;i < MAX_SAMPLES;i++)
+	for (int i = 0;i < sampleAmount;i++)
 	{
-		for (int j = 0;j < MAX_SAMPLES;j++)
-		{
-			for (int k = 0;k < MAX_SAMPLES;k++)
-			{
-				printf("The sample list we generated is:\n");
-				printf("%15d", samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].x);
-				printf("%15d", samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].y);
-				printf("%15d", samplePointsList[i*MAX_SAMPLES*MAX_SAMPLES + j*MAX_SAMPLES + k].z);
-				printf("\n");
-			}
-		}
+		printf("The sample list we generated is:\n");
+		printf("%15d", samplePointsList[i].x);
+		printf("%15d", samplePointsList[i].y);
+		printf("%15d", samplePointsList[i].z);
+		printf("\n");
 	}
 }
--- HoudiniWork1/FastPoissonDisk.h
+++ HoudiniWork1/FastPoissonDisk.h
@@ -15,15 +15,10 @@ class FastPoissonDisk{
 public:
 	FastPoissonDisk();
 
-	int SearchActiveList(int i, int j, int k);
-
-	Eigen::Vector3f GenerateOriginalSamplePoints();
-
-	Eigen::Vector3f GenerateOtherSamples(Eigen::Vector3f testPoint);
-
-	void InsertIntoGridAndSampleList(Eigen::Vector3f newSamplePoint, int sampleCount);
-
 	void GenerationProcess();
+
+	void PrintSampleLocations();
+
 
 	//use to test whether there is any sample inside this cell
 	int grid[MAX_SAMPLES*MAX_SAMPLES*MAX_SAMPLES];
@@ -31,12 +26,27 @@ public:
 	//used to store the generate samples
 	Eigen::Vector3f samplePointsList[MAX_SAMPLES*MAX_SAMPLES*MAX_SAMPLES];
 
+	int activeList[MAX_SAMPLES*MAX_SAMPLES*MAX_SAMPLES];
+
+
+protected:
+	Eigen::Vector3f GenerateOriginalSamplePoints();
+
+	Eigen::Vector3f GenerateOtherSamples(Eigen::Vector3f testPoint);
+
+	void InsertIntoGridAndSampleList(Eigen::Vector3f newSamplePoint, int sampleCount);
+
+	Eigen::Vector3i GridLocation(Eigen::Vector3f newSamplePoint);
+
+	int GridArrayIndex(Eigen::Vector3i gridLocation);
+
+	int SearchActiveList(int i, int j, int k);
+
 	//since we will place the grid between (0,0,0) and (1,1,1), so r should be
-	float radius = sqrt(3.0) / 50.0;
+	float radius = sqrt(DIMENSIONS_N) / MAX_SAMPLES;
 
 	//therefore the cell width would be
-	float cellWidth = 0.02;
-
+	float cellWidth = 1/MAX_SAMPLES;
 };
 
 
